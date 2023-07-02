d09e066115fdf8e5c23aa8e27cc28862bae04371
donvine mugendi <donvinemugendi@gmail.com>
2023-07-03 00:09:01 +0300

    passed tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
+class CashRegister
+    attr_accessor :discount
+    attr_writer :total
 
+    def initialize(discount = 0)
+      @discount = discount
+      @total = 0
+      @items = []
+      @item_prices = []
+    end
+  
+    def total
+      @total
+    end
+  
+    def add_item(title, price, quantity = 1)
+    @quantity = quantity
+      quantity.times do
+        @items << title
+        @item_prices << price.to_f
+      end
+      @total += price.to_f * quantity
+    end
+  
+    def apply_discount
+      if discount > 0
+        total_discount = @discount.to_f / 100 * @total
+        @total -= total_discount
+        "After the discount, the total comes to $#{total.to_i}."
+      else
+        "There is no discount to apply."
+      end
+    end
+  
+    def items
+      @items
+    end
+    def void_last_transaction
+        last_price = @item_prices.pop
+        @total -= last_price * @quantity
+      end
+  end
+  
